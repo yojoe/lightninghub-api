c7022ae7af6cd704864a4d64d54ed234e7b1a787
yojoe <58518123+yojoe@users.noreply.github.com>
2019-12-15 18:36:54 +0100

    move generated index.html to /docs

--- scripts/raml2html.sh
+++ scripts/raml2html.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # requires raml2html in path https://github.com/raml2html/raml2html
-raml2html ../raml10/api.raml > ../generated/html/index.html
+raml2html ../raml10/api.raml > ../docs/index.html
