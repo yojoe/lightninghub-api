362c665674d2f3c839be71dbf832a64e5bb885dc
yojoe <58518123+yojoe@users.noreply.github.com>
2019-12-16 12:35:32 +0100

    generate go server example without api docs

--- scripts/raml2go-server.sh
+++ scripts/raml2go-server.sh
@@ -4,4 +4,5 @@
 mkdir -p ../generated/go/server 2>/dev/null
 go-raml -d server -l go --dir ../generated/go/server \
                --ramlfile ../raml10/api.raml \
-               --import-path github.com/yojoe/lightninghub-api/go/server
+               --import-path github.com/yojoe/lightninghub-api/go/server \
+               --no-apidocs
